c12c83f080dbbe360db6b1acd6fe3f61c3802020
Himanshu  Sharma <54438078+himanshusharma-1@users.noreply.github.com>
2024-09-18 08:04:20 -0400

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -1,8 +1,9 @@
 import streamlit as st
+import requests
 from snowflake.snowpark.functions import col
  
 # Write directly to the app
-st.title(":cup_with_straw: Customize your smoothie :cup_with_straw:")
+st.title("My Parents New Healthy Diner")
 st.write(
     """Choose the fruits you want in your custom Smoothie!.
     """
@@ -14,7 +15,10 @@ st.write('The name on your Smoothie will be:', name_on_order)
 # Get session and fetch data
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+pd_df=my_dataframe.to_pandas()
+# st.dataframe(data=my_dataframe, use_container_width=True)
+# st.stop()
  
 # Ingredient selection
 ingredients_list = st.multiselect("Choose up to 5 ingredients:", my_dataframe, max_selections = 5)
@@ -22,6 +26,14 @@ ingredients_list = st.multiselect("Choose up to 5 ingredients:", my_dataframe, m
 if ingredients_list:
     ingredients_string = ' '.join(ingredients_list)
     st.write("Your selected ingredients:", ingredients_string)
+ 
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ''
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        st.subheader(fruit_chosen + 'Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
  
     # SQL insert statement
     my_insert_stmt = f"INSERT INTO smoothies.public.orders (ingredients, name_on_order) VALUES ('{ingredients_string}','{name_on_order}')"
