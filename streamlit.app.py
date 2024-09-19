a0e20c02e48c8fd8af173126ac25e39c9c49d60c
Himanshu  Sharma <54438078+himanshusharma-1@users.noreply.github.com>
2024-09-19 07:28:27 -0400

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -1,9 +1,11 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 # Write directly to the app
-st.title(":cup_with_straw: Customize your smoothie :cup_with_straw:")
+st.title("My Parents New Healthy Diner")
 st.write(
-    """Choose the fruits you want in your custom Smoothie!.
+    """Breakfast Menu
+ 
     """
 )
 name_on_order = st.text_input('Name on smoothie:')
@@ -11,11 +13,27 @@ st.write('The name on your Smoothie will be:', name_on_order)
 # Get session and fetch data
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-# Ingredient selection
-ingredients_list = st.multiselect("Choose up to 5 ingredients:", my_dataframe, max_selections = 5)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+ 
+pd_df=my_dataframe.to_pandas()
+ 
+ingredients_list = st.multiselect(
+    'Choose up to 5 ingredients:'
+    , my_dataframe
+    , max_selections = 5
+)
+ 
 if ingredients_list:
-    ingredients_string = ' '.join(ingredients_list)
+    ingredients_string = ''
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+ 
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+ 
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+        fv_df= st.dataframe(data=fruityvice_response.json(),use_container_width=True)
     st.write("Your selected ingredients:", ingredients_string)
     # SQL insert statement
     my_insert_stmt = f"INSERT INTO smoothies.public.orders (ingredients, name_on_order) VALUES ('{ingredients_string}','{name_on_order}')"
