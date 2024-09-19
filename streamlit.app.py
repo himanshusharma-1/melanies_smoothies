5ac9cd6bc4ca8c6c989bcb8b07001ae7e9f5febe
Himanshu  Sharma <54438078+himanshusharma-1@users.noreply.github.com>
2024-09-19 07:23:34 -0400

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -1,32 +1,22 @@
 import streamlit as st
-import requests
-import pandas as pd
 from snowflake.snowpark.functions import col
 # Write directly to the app
-st.title(":cup_with_straw: Custom Smoothie Orders :cup_with_straw:")
+st.title(":cup_with_straw: Customize your smoothie :cup_with_straw:")
 st.write(
-    """Choose the fruits you want in your custom Smoothie!."""
+    """Choose the fruits you want in your custom Smoothie!.
+    """
 )
 name_on_order = st.text_input('Name on smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 # Get session and fetch data
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
-pd_df = my_dataframe.to_pandas()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # Ingredient selection
-ingredients_list = st.multiselect("Choose up to 5 ingredients:", pd_df['FRUIT_NAME'].tolist(), max_selections=5)
+ingredients_list = st.multiselect("Choose up to 5 ingredients:", my_dataframe, max_selections = 5)
 if ingredients_list:
-    ingredients_string = ', '.join(ingredients_list)
+    ingredients_string = ' '.join(ingredients_list)
     st.write("Your selected ingredients:", ingredients_string)
-    for fruit_chosen in ingredients_list:
-        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        if not search_on:
-            st.error(f"Search term for {fruit_chosen} is missing.")
-            continue
-        st.subheader(fruit_chosen + ' Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ search_on)
-        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     # SQL insert statement
     my_insert_stmt = f"INSERT INTO smoothies.public.orders (ingredients, name_on_order) VALUES ('{ingredients_string}','{name_on_order}')"
     # Button with a unique key to avoid the DuplicateWidgetID issue
